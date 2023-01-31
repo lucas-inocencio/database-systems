e7382b5e95016f084b4584938a6f49352ca34025
LucasInocencio <113804674+lucas-inocencio@users.noreply.github.com>
2023-01-30 23:09:23 -0300

    Add files via upload

---
+++ API/Produto.js
@@ -0,0 +1,11 @@
+class  Produto{
+  constructor(Cd_Produto,Nm_Produto,Nm_Cor,Nm_Cidade,Qt_Peso){
+    this.Cd_Produto = Cd_Produto;
+    this.Nm_Produto = Nm_Produto;
+    this.Nm_Cor = Nm_Cor;
+    this.Nm_Cidade = Nm_Cidade;
+    this.Qt_Peso = Qt_Peso;
+  }
+}
+ 
+module.exports = Produto;
